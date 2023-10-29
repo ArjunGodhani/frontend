3f32bd10c125995198b48dfc8d9e06f9ea0bd25f
Arjun Patel <111860713+ArjunGodhani@users.noreply.github.com>
2023-10-29 18:30:39 +0530

    2inputs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import List from "./components/List";
+import axios from "axios";
+import { baseURL } from "./utils/constant";
 
-function App() {
+const App = () => {
+  const [input, setInput] = useState("");
+  const [input2, setInput2] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const [updateUI, setUpdateUI] = useState(false);
+  const [updateId, setUpdateId] = useState(null);
+
+  useEffect(() => {
+    axios.get(`${baseURL}/get`).then((res) => {
+      setTasks(res.data);
+    });
+  }, [updateUI]);
+
+  const addTask = () => {
+    const combinedTask = `${input} ${input2}`.trim(); // Combine both inputs
+    axios.post(`${baseURL}/save`, { task: combinedTask }).then(() => {
+      setInput("");
+      setInput2(""); // Clear the second input as well
+      setUpdateUI((prevState) => !prevState);
+    });
+  };
+
+  const updateMode = (id, text) => {
+    setInput(text);
+    setUpdateId(id);
+  };
+
+  const updateTask = () => {
+    axios.put(`${baseURL}/update/${updateId}`, { task: input }).then(() => {
+      setUpdateUI((prevState) => !prevState);
+      setUpdateId(null);
+      setInput("");
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <main className="main">
+        <h1 className="title">CRUD Operations</h1>
+        <div className="input_holder">
+          <input
+            className="myPadding"
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <input
+            className="myPadding"
+            type="text"
+            value={input2}
+            onChange={(e) => setInput2(e.target.value)}
+          />
+          <button className="myPadding" type="submit" onClick={updateId ? updateTask : addTask}>
+            {updateId ? "Update Task" : "Add Task"}
+          </button>
+        </div>
+
+        <ul>
+          {tasks.map((task) => (
+            <List
+              key={task._id}
+              id={task._id}
+              task={task.task}
+              setUpdateUI={setUpdateUI}
+              updateMode={updateMode}
+            />
+          ))}
+        </ul>
+      </main>
+    </>
+
   );
-}
+};
 
 export default App;
